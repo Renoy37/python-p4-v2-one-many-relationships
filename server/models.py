efcbf0362cdcb66bcfbbdaa27d98b513796bb86c
Joy Kithinji Gatwiri <jgatwiri@jacarandahealth.org>
2024-04-05 22:50:19 +0300

    Completed Assignment

--- server/models.py
+++ server/models.py
@@ -11,28 +11,20 @@ metadata = MetaData(
 
 db = SQLAlchemy(metadata=metadata)
 
-
 class Employee(db.Model):
-    __tablename__ = "employees"
+    __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
-    def __repr__(self):
-        return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
+    reviews = db.relationship('Review', back_populates="employee", cascade='all, delete-orphan')
 
-
-class Onboarding(db.Model):
-    __tablename__ = "onboardings"
-
-    id = db.Column(db.Integer, primary_key=True)
-    orientation = db.Column(db.DateTime)
-    forms_complete = db.Column(db.Boolean, default=False)
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee', cascade='all, delete-orphan')
 
     def __repr__(self):
-        return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
-
+        return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
 class Review(db.Model):
     __tablename__ = "reviews"
@@ -40,6 +32,9 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    employee = db.relationship('Employee', back_populates='reviews')
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
